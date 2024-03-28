a28ba64eb5c9e515b0d432d5a167242f4025c10b
wo-r <78428114+wo-r@users.noreply.github.com>
2024-03-28 10:20:55 -0500

    make the service worker not cache

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,6 @@
-self.addEventListener('install', function(e) {
-    e.waitUntil(
-        caches.open('Proview').then(function(cache) {
-            return cache.addAll([
-                "/",
-                "index.html",
-                "app.js",
-                "jquery.js",
-                "tailwind.js"
-            ]);
-        })
-    );
-});
+self.addEventListener('install', function(event) {});
+self.addEventListener('activate', function(event) {});
 
 self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
-        })
-    );
+  event.respondWith(fetch(event.request));
 });
