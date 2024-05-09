c7d31a1c893c52459b90b8e44dbd20e3a1a3c524
wo-r <78428114+wo-r@users.noreply.github.com>
2024-05-09 14:34:20 -0500

    fix manifest

--- src/pages/login.js
+++ src/pages/login.js
@@ -113,8 +113,4 @@ export async function run() {
             });
         }
     })
-
-    var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-    var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-    $("html, body").css({"width":w,"height":h});
 }
